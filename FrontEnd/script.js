1c8d7a52231c7a93ccdc7be99d63a387a8fdd8fe
scheveny <scheveny@gmail.com>
2023-07-03 18:27:11 +0200

    feat(projects-dynamic-add):script.js+index.html

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,39 @@
 async function fetchProjects() {
-      const response = await fetch('http://localhost:5678/api/');
-      const dataProjects = await response.json();
-      const gallery = document.querySelector('.gallery');
+  try {
+    let response = await fetch('http://localhost:5678/api/works');
+    let data = await response.json();
 
-  
-      for (let i = 0; i < dataProjects.length; i++) {
-        const project = dataProjects[i];
+    return data;
+  } catch (error) {
+    console.error('Une erreur s\'est produite :', error);
+    return []; // Retourner un tableau vide en cas d'erreur
+  }
+}
 
-        // Créer les éléments HTML pour chaque projet
-        const galleryProject = document.createElement('figure');
-        const imageProject = document.createElement('img');
-        const titleProject = document.createElement('figcaption');
-  
-        // Définir les attributs et le contenu des éléments
-        imageProject.src = project.image;
-        titleProject.textContent = project.title;
+async function displayProjects() {
+  let gallery = document.querySelector('.gallery');
+  let dataProjects = await fetchProjects();
 
-        // Ajouter les éléments à la galerie
-        gallery.appendChild(galleryProject);
-        galleryProject.appendChild(imageProject);
-        galleryProject.appendChild(titleProject)
-      };
-    }
+  for (let i = 0; i < dataProjects.length; i++) {
+    let project = dataProjects[i];
 
+    // Créer les éléments HTML pour chaque projet
+    let galleryProject = document.createElement('figure');
+    let imageProject = document.createElement('img');
+    let titleProject = document.createElement('figcaption');
+
+    // Définir les attributs et le contenu des éléments
+    imageProject.src = project.imageUrl;
+    titleProject.textContent = project.title;
+
+    // Ajouter les éléments à la galerie
+    gallery.appendChild(galleryProject);
+    galleryProject.appendChild(imageProject);
+    galleryProject.appendChild(titleProject);
+  }
+}
+
+// Pour s'assurer que le contenu de la page est complètement chargé avant d'appeler displayProjects()
+document.addEventListener('DOMContentLoaded', () => {
+  displayProjects();
+});
