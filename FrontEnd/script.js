7cfadc17e5f6edefb72c0354ee4d1343e2daff12
scheveny <scheveny@gmail.com>
2023-07-04 15:04:59 +0200

    feat(trier-buttons+style-homepage):html+css+js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,34 +6,64 @@ async function fetchProjects() {
     return data;
   } catch (error) {
     console.error('Une erreur s\'est produite :', error);
-    return []; // Retourner un tableau vide en cas d'erreur
+    return [];
   }
 }
 
 async function displayProjects() {
   let gallery = document.querySelector('.gallery');
   let dataProjects = await fetchProjects();
+  let categoryButtons = document.querySelectorAll('.category-buttons button');
 
-  for (let i = 0; i < dataProjects.length; i++) {
-    let project = dataProjects[i];
-
-    // Créer les éléments HTML pour chaque projet
-    let galleryProject = document.createElement('figure');
-    let imageProject = document.createElement('img');
-    let titleProject = document.createElement('figcaption');
-
-    // Définir les attributs et le contenu des éléments
-    imageProject.src = project.imageUrl;
-    titleProject.textContent = project.title;
-
-    // Ajouter les éléments à la galerie
-    gallery.appendChild(galleryProject);
-    galleryProject.appendChild(imageProject);
-    galleryProject.appendChild(titleProject);
+  // Filtrer les projets en fonction de la catégorie sélectionnée
+  function filterProjects(category) {
+    if (category === 'all') {
+      return dataProjects;
+    } else {
+      return dataProjects.filter(project => project.category.id === parseInt(category));
+    }
   }
+
+  // Afficher les projets dans la galerie
+  function showProjects(projects) {
+    gallery.innerHTML = '';
+
+    for (let i = 0; i < projects.length; i++) {
+      let project = projects[i];
+
+      let galleryProject = document.createElement('figure');
+      let imageProject = document.createElement('img');
+      let titleProject = document.createElement('figcaption');
+
+      imageProject.src = project.imageUrl;
+      titleProject.textContent = project.title;
+
+      gallery.appendChild(galleryProject);
+      galleryProject.appendChild(imageProject);
+      galleryProject.appendChild(titleProject);
+    }
+  }
+
+  // Afficher tous les projets
+  function showAllProjects() {
+    showProjects(dataProjects);
+  }
+
+  // Gérer les clics sur les boutons de catégorie
+  for (let i = 0; i < categoryButtons.length; i++) {
+    let button = categoryButtons[i];
+
+    button.addEventListener('click', () => {
+      let selectedCategory = button.dataset.category;
+      let filteredProjects = filterProjects(selectedCategory);
+      showProjects(filteredProjects);
+    });
+  }
+
+  // Afficher tous les projets au chargement initial
+  showAllProjects();
 }
 
-// Pour s'assurer que le contenu de la page est complètement chargé avant d'appeler displayProjects()
 document.addEventListener('DOMContentLoaded', () => {
   displayProjects();
 });
