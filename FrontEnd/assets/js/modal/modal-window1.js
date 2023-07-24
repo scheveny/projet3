1d7a4ba02ce5c4002ff0815509c4fb1456f6cb21
scheveny <scheveny@gmail.com>
2023-07-24 21:17:58 +0200

    feat(deleteAllProjects-button):modal-window1.js

--- FrontEnd/assets/js/modal/modal-window1.js
+++ FrontEnd/assets/js/modal/modal-window1.js
@@ -32,9 +32,18 @@ function showProjects(dataProjects) {
     deleteIcon.style.width = '17px';
     deleteIcon.style.height = '17px';
 
+    // Delete a project by id button
+
     deleteIcon.addEventListener('click', () => {
       deleteProject(project.id);
       fetchProjects().then(updatedProjects => showProjects(updatedProjects));
+    });
+
+    // Delete all gallery projects button
+
+    const galleryDeleteBtn = document.querySelector('.gallery-delete-btn');
+    galleryDeleteBtn.addEventListener('click', async () => {
+     await deleteAllProject('.modal-gallery figure');
     });
   }
 }
@@ -44,7 +53,7 @@ async function deleteProject(projectId) {
   const authToken = sessionStorage.getItem('authToken');
 
   try {
-    const response = await fetch(`http://localhost:5678/api/works/${projectId}`, {
+    let response = await fetch(`http://localhost:5678/api/works/${projectId}`, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${authToken}`,
@@ -56,15 +65,45 @@ async function deleteProject(projectId) {
       throw new Error('Réponse réseau erronée');
     }
 
-    // Handle the success response here if needed
-    console.log('Le projet a été supprimé avec succès');
   } catch (error) {
     // Handle any errors that occurred during the fetch request
     console.error('Erreur dans la suppression du projet:', error);
+  }};
+
+async function deleteAllProject () {
+
+  const authToken = sessionStorage.getItem('authToken');
+
+  try {
+    const gallery = document.querySelector('.modal-gallery');
+    const elementsToDelete = gallery.querySelectorAll(`.modal-gallery figure`);
+
+    for (const element of elementsToDelete) {
+      const response = await fetch(`http://localhost:5678/api/works/${element.dataset.projectId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        console.error('Erreur lors de la suppression des projets');
+      } else {
+        console.log('Tous les projets ont été supprimés avec succès');
+      }
+    }
+
+    // If needed, handle the success response here
+  } catch (error) {
+    console.error('Erreur lors de la suppression des projets:', error);
   }
 }
 
 showProjects(dataProjects);
+
+
+
+
 
 // Pathway between modal gallery delete projects and modal window add projects
 
@@ -76,13 +115,4 @@ btn.addEventListener('click', () => {
 
   modal1.style.display = 'none';
   modal2.style.display = 'block';
-});
-
-// Delete all projects button
-
-let deleteBtn = document.querySelector('.gallery-delete-btn');
-
-deleteBtn.addEventListener('click', () => {
-  let projectList = document.querySelector('.modal-gallery figure');
-  projectList.innerHTML = '';
 });
