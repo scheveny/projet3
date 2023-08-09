e64dc4592090bb92ef62a84de1102a339ca007a7
scheveny <scheveny@gmail.com>
2023-08-10 00:07:23 +0200

    fix(delete and add projects without reloading):js

--- FrontEnd/assets/js/modal/OpenCloseModal.js
+++ FrontEnd/assets/js/modal/OpenCloseModal.js
@@ -1,26 +1,165 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { fetchProjects } from "../fetch.js";
 
+document.addEventListener('DOMContentLoaded', async () => {
     let editProjectLink = document.querySelector('#edit-project-modal');
     let closeModalButtons = document.querySelectorAll('.close-btn');
     let editProjectModals = document.querySelectorAll('.modal-wdws');
+    let modalGallery = document.querySelector('.modal-gallery');
+
+    async function deleteProject(projectId) {
+        const authToken = sessionStorage.getItem('authToken');
+        try {
+            await fetch(`http://localhost:5678/api/works/${projectId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${authToken}`,
+                },
+            });
+
+            let classToDelete = '.project-gallery-' + projectId;
+            console.log('elementtodelete : ' + classToDelete)
+            let elementToDelete = document.querySelectorAll(classToDelete);
+            console.log(elementToDelete)
+            elementToDelete.forEach(element => {
+                element.remove();
+            });
+    
+            console.log('Projet supprimé avec succès.');
+
+            // Mise à jour de la galerie après la suppression
+            let updatedProjects = await fetchProjects();
+            removeAllDeleteListeners();
+            showModalProjects(updatedProjects);
+
+    
+        } catch (error) {
+            console.error('Erreur dans la suppression du projet individuel:', error);
+            throw error; // Répéter l'erreur pour que l'appelant (l'écouteur d'événements) puisse également la gérer.
+        }
+    }
+    
+    async function deleteAllProject() {
+        const authToken = sessionStorage.getItem('authToken');
+        try {
+            const allProjects = await fetchProjects();
+
+            // Supprimer chaque projet individuellement en utilisant son ID
+            const deleteRequests = allProjects.map((project) => {
+            return fetch(`http://localhost:5678/api/works/${project.id}`, {
+                method: 'DELETE',
+                headers: {
+                'Authorization': `Bearer ${authToken}`,
+                },
+            });
+            });
+
+            // Attendre que toutes les requêtes de suppression soient terminées
+            await Promise.all(deleteRequests);
+
+            let elementToDelete = document.querySelectorAll('figure');
+            elementToDelete.forEach(element => {
+            element.remove();
+            })
+
+            // Suppression réussie, mettre à jour la galerie
+            let updatedProjects = await fetchProjects();
+            modalGallery.innerHTML = '';
+            showModalProjects(updatedProjects);
+            console.log('Tous les projets ont été supprimés avec succès.');
+
+
+        } catch (error) {
+            console.error('Une erreur est survenue lors de la suppression des projets :', error);
+            throw error; // Répéter l'erreur pour que l'appelant (l'écouteur d'événements) puisse également la gérer.
+        }
+    }
+
+    function removeAllDeleteListeners() {
+        const deleteIcons = document.querySelectorAll('.project-gallery img[src="assets/icons/delete-icon.png"]');
+        deleteIcons.forEach(icon => {
+            const newIcon = icon.cloneNode(true);
+            icon.parentNode.replaceChild(newIcon, icon);
+        });
+    }
+
+    function addDeleteListener(deleteIcon, projectId) {
+        deleteIcon.addEventListener('click', async () => {
+            try {
+                await deleteProject(projectId);
+                let updatedProjects = await fetchProjects();
+                removeAllDeleteListeners();
+                showModalProjects(updatedProjects);
+            } catch (error) {
+                console.error('Erreur dans la suppression du projet individuel :', error);
+            }
+        });
+    }
+
+    function showModalProjects(galleryData) {
+        modalGallery.innerHTML = '';
+
+        for (let i = 0; i < galleryData.length; i++) {
+            let project = galleryData[i];
+
+            let galleryProject = document.createElement('figure');
+            galleryProject.classList.add('project-gallery-' + project.id);
+            let imageContainer = document.createElement('div');
+            let imageProject = document.createElement('img');
+            let titleProject = document.createElement('figcaption');
+            let deleteIcon = document.createElement('img'); 
+
+            imageProject.src = project.imageUrl;
+            titleProject.innerText = 'éditer';
+            deleteIcon.src = 'assets/icons/delete-icon.png';
+
+            modalGallery.appendChild(galleryProject);
+            galleryProject.appendChild(imageContainer);
+            imageContainer.appendChild(imageProject);
+            imageContainer.appendChild(deleteIcon);
+            galleryProject.appendChild(titleProject);
+
+            imageContainer.style.position = 'relative';
+            deleteIcon.style.position = 'absolute';
+            deleteIcon.style.top = '5px';
+            deleteIcon.style.right = '5px';
+            deleteIcon.style.width = '17px';
+            deleteIcon.style.height = '17px';
+
+            addDeleteListener(deleteIcon, project.id);
+        }
+
+        let galleryDeleteBtn = document.querySelector('.gallery-delete-btn');
+        galleryDeleteBtn.addEventListener('click', async () => {
+            try {
+                await deleteAllProject();
+                modalGallery.innerHTML = '';
+            } catch (error) {
+                console.error('Une erreur est survenue lors de la suppression des projets :', error);
+            }
+        });
+    }
+
+    function closeModal(event) {
+        event.preventDefault();
+        let modalToClose = event.target.closest('.modal-wdws');
+        modalToClose.style.display = 'none';
+    }
 
     function openModal(event) {
         event.preventDefault();
         editProjectModals.forEach((modal) => {
             modal.style.display = 'flex';
         });
-    };
+        fetchProjects()
+        .then(galleryData => {
+            showModalProjects(galleryData);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+        });
+    }
 
-    function closeModal(event) {
-        event.preventDefault();
-        let modalToClose = event.target.closest('.modal-wdws');
-            modalToClose.style.display = 'none';
-
-    };
-
-    editProjectLink.addEventListener("click", (event) => {
-        openModal(event);
-    });
+    editProjectLink.addEventListener("click", openModal);
 
     closeModalButtons.forEach(function (button) {
         button.addEventListener('click', closeModal);
@@ -31,5 +170,36 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal(event);
         }
     });
+
+    // Back button function
+
+        let backBtn = document.querySelector('.back-btn');
+
+        backBtn.addEventListener('click', async () => {
+            let modal1 = document.querySelector('.modal-wdw1');
+            let modal2 = document.querySelector('.modal-wdw2');
+
+            modal1.style.display = 'flex';
+            modal2.style.display = 'none';
+
+            try {
+                let updatedProjects = await fetchProjects();
+                modalGallery.innerHTML = '';
+                showModalProjects(updatedProjects);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données :', error);
+            }
+        });
 });
 
+// Pathway between delete-project wdw1 and add-project wdw2
+
+let btn = document.querySelector('.pjct-add-btn');
+
+btn.addEventListener('click', () => {
+  let modal1 = document.querySelector('.modal-wdw1');
+  let modal2 = document.querySelector('.modal-wdw2');
+
+  modal1.style.display = 'none';
+  modal2.style.display = 'block';
+});
--- FrontEnd/assets/js/modal/add-project.js
+++ FrontEnd/assets/js/modal/add-project.js
@@ -1,15 +1,3 @@
-// Back button function
-
-let backBtn = document.querySelector('.back-btn');
-
-backBtn.addEventListener('click', () => {
-  let modal1 = document.querySelector('.modal-wdw1');
-  let modal2 = document.querySelector('.modal-wdw2');
-
-  modal1.style.display = 'flex';
-  modal2.style.display = 'none';
-});
-
 // Showing img after selecting it in file-input
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -35,87 +23,79 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Upload form
 
-// Close the modal after submission
-function closeModal() {
-  let modal = document.querySelector('.project-modal');
-  modal.style.display = 'none';
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   
-let uploadForm = document.getElementById('submit-project');
-let gallery = document.querySelector('.gallery');
-const authToken = sessionStorage.getItem('authToken');
+  let uploadForm = document.getElementById('submit-project');
+  let gallery = document.querySelector('.gallery');
+  const authToken = sessionStorage.getItem('authToken');
 
-uploadForm.addEventListener('click', async () => {
+  uploadForm.addEventListener('click', async () => {
 
-  // Récupérez le fichier sélectionné par l'utilisateur
-  let imageFileInput = document.getElementById('file-input');
-  let imageUrl = imageFileInput.files[0]; // Le fichier sélectionné est le premier élément du tableau 'files'
+    // Récupérez le fichier sélectionné par l'utilisateur
+    let imageFileInput = document.getElementById('file-input');
+    let imageUrl = imageFileInput.files[0]; // Le fichier sélectionné est le premier élément du tableau 'files'
 
-  // Récupérez les autres données du formulaire si nécessaire
-  let title = document.getElementById('title').value;
-  let categoryId = document.getElementById('categoryId').value;
+    // Récupérez les autres données du formulaire si nécessaire
+    let title = document.getElementById('title').value;
+    let categoryId = document.getElementById('categoryId').value;
 
-  // Validate the form data
-  if (!imageUrl || !title) {
-    alert('Veuillez remplir tous les champs du formulaire.');
-    return; // Stop the form submission if any field is missing
-  }
-
-  // Créez un objet FormData pour envoyer les données au serveur
-  let formData = new FormData();
-  formData.append('image', imageUrl);
-  formData.append('title', title);
-  formData.append('category', categoryId);
-
-  try {
-    // Envoyez les données au serveur en utilisant 'fetch'
-    let response = await fetch('http://localhost:5678/api/works', {
-      method: 'POST',
-      body: formData, // Utilisez le FormData comme corps de la requête
-      headers: {
-        'Authorization': `Bearer ${authToken}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Erreur lors de l\'envoi des données.');
+    // Validate the form data
+    if (!imageUrl || !title) {
+      alert('Veuillez remplir tous les champs du formulaire.');
+      return; // Stop the form submission if any field is missing
     }
 
-    let data = await response.json(); // Supposons que le serveur renvoie une réponse JSON
-    console.log('Réponse du serveur :', data);
+    // Créez un objet FormData pour envoyer les données au serveur
+    let formData = new FormData();
+    formData.append('image', imageUrl);
+    formData.append('title', title);
+    formData.append('category', categoryId);
 
-    let event = new CustomEvent('projectAdded', { detail: data });
-    document.dispatchEvent(event);
+    try {
+      // Envoyez les données au serveur en utilisant 'fetch'
+      let response = await fetch('http://localhost:5678/api/works', {
+        method: 'POST',
+        body: formData, // Utilisez le FormData comme corps de la requête
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Erreur lors de l\'envoi des données.');
+      }
 
-    closeModal();
+      let data = await response.json(); // Supposons que le serveur renvoie une réponse JSON
+      console.log('Réponse du serveur :', data);
 
-  } catch (error) {
-    console.error('Erreur lors de l\'envoi des données :', error);
-  }
-});
+      let event = new CustomEvent('projectAdded', { detail: data });
+      document.dispatchEvent(event);
 
-document.addEventListener('projectAdded', function(event) {
-  let data = event.detail;
-  let newProject = createModalProject(data);
-  gallery.appendChild(newProject);
-});
+      } catch (error) {
+        console.error('Erreur lors de l\'envoi des données :', error);
+      }
+  });
+      document.addEventListener('projectAdded', function(event) {
+        let data = event.detail;
+        let newProject = createModalProject(data);
+        gallery.appendChild(newProject);
+      });
 
 
-function createModalProject(project) {
-  let galleryProject = document.createElement('figure');
-  let imageProject = document.createElement('img');
-  let titleProject = document.createElement('figcaption');
+      function createModalProject(project) {
+        let galleryProject = document.createElement('figure');
+        galleryProject.classList.add('project-gallery-' + project.id);
+        let imageProject = document.createElement('img');
+        let titleProject = document.createElement('figcaption');
 
-  imageProject.src = project.imageUrl;
-  titleProject.textContent = project.title;
+        imageProject.src = project.imageUrl;
+        titleProject.textContent = project.title;
 
-  gallery.appendChild(galleryProject);
-  galleryProject.appendChild(imageProject);
-  galleryProject.appendChild(titleProject);
+        gallery.appendChild(galleryProject);
+        galleryProject.appendChild(imageProject);
+        galleryProject.appendChild(titleProject);
 
-  return galleryProject;
-}
+        return galleryProject;
+      }
 });
 
 // Check size of the upload image
